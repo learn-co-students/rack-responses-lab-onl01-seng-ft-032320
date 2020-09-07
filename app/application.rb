3d9f0e155a7af9c23fe9b08585eb0712080ccb09
javante_nelson <javantenelson@gmail.com>
2020-09-07 09:48:04 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+
+    def call(env)
+      resp = Rack::Response.new
+      # resp.write "Hello, World"
+  
+      time = Time.now
+
+
+      if time.hour < 12 
+        resp.write "Good Morning!"
+      else
+        resp.write "Good Afternoon!"
+      end
+  
+      resp.finish
+    end
+  
+  end
